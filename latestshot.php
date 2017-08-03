9d0715091f2c78ca143db006fe2d0738f4fa8bc7
Sasha Shuplat <kavabata.y@gmail.com>
2017-08-03 17:43:55 +0200

    Report page ready

--- latestshot.php
+++ latestshot.php
@@ -2,6 +2,7 @@
 
 $path = '/home/pi/html/cam/';
 $folder = date('Ymd');
+$v = date('Ymd', strtotime('-1 day'));
 $dir = $path . $folder;
 
 if (is_dir($dir)) {
@@ -20,5 +21,7 @@ if (is_dir($dir)) {
 
 $shot['src'] = 'http://fastaccess.ddns.net/cam/' . $folder . '/'. $resultname . '.jpg';
 $shot['title'] = 'Taken on ' .substr($resultname,0,2) . ':' . substr($resultname,2);
+
+$video['href'] = 'http://fastaccess.ddns.net/cam/' . $v . '.mp4';
 
 
